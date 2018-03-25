7735be068bb495df913627c401fb8bc7a2861496
tntloverlol <ethankuehnel@gmail.com>
2018-03-25 15:16:13 -0600

    first commit

---
+++ WpfApp1/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WpfApp1.Models
+{
+    class User
+    {
+        [BsonId]
+//        [BsonRepresentation(BsonType.ObjectId)]
+        public string Username;// { get; }
+        public string Password;// { private get; set; }
+
+        public User()
+        {
+        }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public bool CheckPassword(string pass)
+        {
+            return Password == pass;
+        }
+    }
+}
